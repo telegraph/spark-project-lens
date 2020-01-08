1dfe23ce07905eb7021f99169ab519f213712155
johnstonx <xander.johnston@telegraph.co.uk>
2020-01-08 11:41:14 +0000

    API redux action

--- src/actions/ApiActions.ts
+++ src/actions/ApiActions.ts
@@ -1,10 +1,13 @@
 import { Dispatch } from 'redux';
 import { FETCH_DATA } from './types';
+import * as API from '../adapters/API'
 
 export const fetchData = () => (dispatch: Dispatch) => {
   // API call goes here
+  const data = API.allCardData()
+
   dispatch({
     type: FETCH_DATA,
-    payload: [], /* fetched data goes here */
+    payload: data, /* fetched data goes here */
   });
 };
