12eeffb2011832f6f2b1ed66e95f1d35be6aa0c9
Alex Hooper <Alex.Hooper@telegraph.co.uk>
2020-01-28 16:31:54 +0000

    Defining task interface

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,15 +1,37 @@
 // Action types
+import { DateTime } from 'luxon';
+
 export const FETCH_DATA = 'FETCH_DATA';
 
 // Action interfaces
 export interface FetchDataAction {
   type: typeof FETCH_DATA;
-  payload: object;
+  payload: Task[];
+}
+
+export interface Task {
+  clientName: string;
+  projectname: string;
+  campaignID: string;
+  currentTask: string;
+  dueDate: DateTime;
+  projectStatus: string;
+  assignedDev: Developer;
+  assignedMembers: [Developer];
+  testLink: string;
+  headshots: [string];
 }
 
 export type ReducerActionTypes = FetchDataAction;
 
 // State interfaces
 export interface ReducerState {
-  tasks: object;
+  tasks: Task[];
+}
+
+export interface Developer {
+  ID: string;
+  avatarDownloadURL: string;
+  name: string;
+  objCode: string;
 }
